a8c7c0fb0057114208fc86a8f75fce511e5b4cff
Rushikesh Bagade <rbagade911@gmail.com>
2025-02-16 12:50:23 +0530

    updated

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -1,22 +1,83 @@
+const User = require("../models/user");
 const { instance } = require("../paymentConfig/razorpayConfig");
-
+const crypto = require("node:crypto");
+exports.getKey = async = (req, res) => {
+  try {
+    res.status(200).json({ key: process.env.RAZORPAY_TEST_ID });
+    // console.log(process.env.RAZORPAY_TEST_ID);
+  } catch (error) {
+    console.log(error);
+  }
+};
 exports.checkout = async (req, res) => {
+  // console.log(req.body);
 
-  console.log(req.body);
-  
+  try {
+    const { email } = req.body;
+    const findUser = await User.findOne({ email: email });
+    const totalUsers = (await User.find().countDocuments()) > 100;
+    let amount = "0";
+    if (findUser.earlySubscriber || totalUsers < 100) {
+      amount = "10000";
+      findUser.earlySubscriber = true;
+      await findUser.save()
+    } else {
+      amount = "20000";
+    }
+    const options = {
+      amount: Number(amount),
+      currency: "INR",
+    };
 
-  const { amount } = req.body
-  const options = {
-    amount: Number(amount * 100),
-    currency: "INR",
-  };
+    const order = await instance.orders.create(options);
 
-  const order = await instance.orders.create(options);
+    // console.log(order);
 
-  // console.log(order);
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  res.status(200).json({
-    success: true,
-    order
-  })
+exports.paymentVerification = async (req, res) => {
+  // console.log(req.body);
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature, email } =
+    req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_SECRET_KEY)
+    .update(body.toString())
+    .digest("hex");
+
+  const findUser = await User.findOne({ email: email });
+
+  console.log(findUser);
+
+  const isAuthenticate = expectedSignature === razorpay_signature;
+
+  const expiriyDate = new Date();
+  expiriyDate.setDate(expiriyDate.getDate() + 30);
+
+  if (isAuthenticate && findUser) {
+    findUser?.payments?.push({
+      order_id: razorpay_order_id,
+      payment_id: razorpay_payment_id,
+      signature: razorpay_signature,
+      amount: 700,
+      status: "Paid",
+      expiresAt: expiriyDate,
+    });
+    findUser.currentExpiryDate = expiriyDate
+    findUser.isSubscribed = true;
+    await findUser.save();
+
+    return res.json({ success: true, refernceid: razorpay_payment_id, expiriyDate: findUser.currentExpiryDate });
+  }
+
+  // res.status(200).json({ sucess: true });
 };
